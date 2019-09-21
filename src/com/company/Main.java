5e30e4265919f88842e9ed219a50bf944c7a2235
usb12345 <54896617+usb12345@users.noreply.github.com>
2019-09-21 15:04:11 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,6 @@ public class Main {
         Employee empl3 = new Employee(5000);
         System.out.println("Enter a salary"  +10000);
         System.out.println("Enter commission"     +10000*0.1);
-        System.out.println("Enter gdhyg"     +5000/4);
+        System.out.println("Enter bonus"     +5000/4);
     }
 }
